34bdaaab9bad90f966b4373496320f1cf8f02b45
Bryan <bryanleon_2687@hotmail.com>
2019-10-24 22:33:48 +0000

    variables2

--- lab1/devops/Jenkinsfile2StageGit.groovy
+++ lab1/devops/Jenkinsfile2StageGit.groovy
@@ -3,12 +3,15 @@ node{
     withEnv(["ENV_CLIENTID=12345677-123456-123456"]){
     stage("init"){ 
         echo "${ENV_CLIENTID}"
+
+        printenv
     }
+
     }
 
 
     stage("checkout"){
-        println "TODO"
-
+        println "==== TODO ===="
+printenv
     }
 }
