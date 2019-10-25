c9881b491242702d589ea8a52448255e7584f1d5
Bryan <bryanleon_2687@hotmail.com>
2019-10-25 00:16:50 +0000

    hola6

--- lab1/devops/Jenkinsfile4Docker.groovy
+++ lab1/devops/Jenkinsfile4Docker.groovy
@@ -1,4 +1,30 @@
 
 node{
-    
+    stage("init"){
+
+    }
+    stage("checkout"){
+        withCredentials([string(credentialsId:"bleon-token",variable:'token1')]){
+            echo "${token1}"
+
+            tokentmp = token1.split('\\|')
+            tok1 = tokentmp[0]
+            tok2 = tokentmp[1]
+
+            echo " ${tok1} ---- ${tok2}"
+
+
+        }
+
+    }
+    stage("deploy"){
+        docker.image("hashicorp/terraform:0.12.12").withRun("-version"){
+            container -> sh "docker logs ${container-id}"
+
+
+        }
+    }
+
+
+
 }
