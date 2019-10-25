536bc79b95ff7f7233b08e94bc3dd923852d25a7
Bryan <bryanleon_2687@hotmail.com>
2019-10-25 00:03:19 +0000

    hola4

--- lab1/devops/Jenkinsfile3Credentials.groovy
+++ lab1/devops/Jenkinsfile3Credentials.groovy
@@ -1,4 +1,25 @@
 
 node{
-    
+    stage("init"){
+
+    }
+    stage("checkou"){
+        withCredentials([string(credentialsId:"bleon-token",variable:"token1")]){
+            echo "${token1}"
+
+            tokentmp = token1.split('\\|')
+            tok1 = tokentmp[0]
+            tok2 = tokentmp[1]
+
+            echo " ${tok1} ---- ${tok2}"
+
+
+        }
+
+    }
 }
+
+/*
+password2257|bleon@outlook.com|textsecretp
+
+/*
